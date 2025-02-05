d01876502fdd2891117bb872f7d1a00dae6b79fb
muriukialex-n <muriukialex.n@gmail.com>
2025-02-05 17:35:14 +0300

    trying

---
+++ Kotlin(Basics)/CodeSiganal/eggCount.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val ingredients = "eggs"
+    var eggsCount = 12  // TODO: This is your starting eggs count.
+
+    println("Ingredients available: $ingredients")
+    println("Number of $ingredients: $eggsCount")
+    
+    var remEggsCount = eggsCount - 3
+    println("Number of eggs left: $remEggsCount")
+
+    // Cooking one omelet uses 3 eggs
+    // TODO: Update the eggs count after cooking one omelet
+    // TODO: Print the new number of eggs left
+}
